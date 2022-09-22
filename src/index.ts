af6311af47de3e4e4d55887f69e21578beffcf9f
Julian Gonzalez <joticajulian@gmail.com>
2022-09-22 15:55:17 +0000

    add more options to the cli

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,12 @@ import { generateProto } from "./generateProto";
 
 async function main() {
   program.option("--all");
+  program.option("--copysrc");
+  program.option("--proto");
+  program.option("--interface");
+  program.option("--abi");
+  program.option("--precompile");
+
   program.parse();
 
   const options = program.opts();
@@ -25,45 +31,74 @@ async function main() {
     class: cclass,
   } = getConfig(program.args[0]);
 
+  if (Object.keys(options).length === 0) {
+    // do all if no options are provided
+    options.all = true;
+  }
+
   if (options.all) {
+    options.copysrc = true;
+    options.proto = true;
+    options.interface = true;
+    options.abi = true;
+    options.precompile = true;
+  }
+
+  if (options.copysrc) {
     // copy source files
     fse.copySync(sourceDir, buildDir);
+    console.log(`source file copied to ${buildDir}`);
+  }
 
+  if (options.proto) {
     // copy koinos protos
     fse.copySync(koinosProtoDir, path.join(buildDir, "proto/koinos"));
 
     // generate proto ts
     generateProto(proto, path.join(buildDir, "proto"));
-    console.log(`files generated at ${buildDir}`);
+    console.log(`proto files generated at ${path.join(buildDir, "proto")}`);
+  }
 
+  if (options.precompile || options.interface || options.abi) {
     const tsStructure = await parseTypescript(files, proto, cclass);
 
-    // prepare subfolders
-    const interfacesDir = path.join(buildDir, "interfaces");
-    if (!fs.existsSync(interfacesDir))
-      fs.mkdirSync(interfacesDir, { recursive: true });
+    if (options.interface) {
+      // prepare subfolders
+      const interfacesDir = path.join(buildDir, "interfaces");
+      if (!fs.existsSync(interfacesDir))
+        fs.mkdirSync(interfacesDir, { recursive: true });
 
-    // generate index file
-    const indexData = generateIndex(tsStructure, buildDir);
-    const outputFileIndex = path.join(buildDir, "index.ts");
-    fs.writeFileSync(outputFileIndex, indexData);
+      // generate interfaces
+      const generateInterfaces = (ts: TsStructure) => {
+        const data = generateInferface(ts, interfacesDir);
+        const outputFile = path.join(interfacesDir, `I${ts.className}.ts`);
+        fs.writeFileSync(outputFile, data);
+        ts.extends.forEach((e) => generateInterfaces(e));
+      };
+      generateInterfaces(tsStructure);
+      console.log(`interfaces generated at ${interfacesDir}`);
+    }
 
-    // generate interfaces
-    const generateInterfaces = (ts: TsStructure) => {
-      const data = generateInferface(ts, interfacesDir);
-      const outputFile = path.join(interfacesDir, `I${ts.className}.ts`);
-      fs.writeFileSync(outputFile, data);
-      ts.extends.forEach((e) => generateInterfaces(e));
-    };
-    generateInterfaces(tsStructure);
+    if (options.abi) {
+      // generate abi
+      const abiData = await generateAbi(tsStructure, proto);
+      const outputFileAbi = path.join(
+        buildDir,
+        `${cclass.toLocaleLowerCase()}-abi.json`
+      );
+      fs.writeFileSync(outputFileAbi, JSON.stringify(abiData, null, 2));
+      console.log(`abi file generated at ${outputFileAbi}`);
+    }
 
-    // generate abi
-    const abiData = await generateAbi(tsStructure, proto);
-    const outputFileAbi = path.join(
-      buildDir,
-      `${cclass.toLocaleLowerCase()}-abi.json`
-    );
-    fs.writeFileSync(outputFileAbi, JSON.stringify(abiData, null, 2));
+    if (options.precompile) {
+      // generate index file
+      const indexData = generateIndex(tsStructure, buildDir);
+      const outputFileIndex = path.join(buildDir, "index.ts");
+      fs.writeFileSync(outputFileIndex, indexData);
+      console.log(
+        `precompilation generated at ${path.join(buildDir, "index.ts")}`
+      );
+    }
   }
 }
 
