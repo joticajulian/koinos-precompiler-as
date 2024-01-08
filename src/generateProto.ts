80d39075996537d757f8b878265adef9f731ad69
Frank <71796683+frank-weijers@users.noreply.github.com>
2024-01-08 23:49:07 +0100

    Make generateProto compatible for Windows

--- src/generateProto.ts
+++ src/generateProto.ts
@@ -2,13 +2,19 @@ import { execSync } from "child_process";
 import { getFiles } from "./utils";
 
 export function generateProto(protoDir: string): void {
+  let protocFile = './node_modules/.bin/as-proto-gen';
+
+  if (process.platform === 'win32') {
+    protocFile = '.\\node_modules\\.bin\\as-proto-gen.cmd';
+  }
+
   const protoFiles = getFiles(protoDir, ".proto");
   protoFiles.map((protoFile) => {
     console.log(protoFile);
     execSync(
       [
         "yarn protoc",
-        "--plugin=protoc-gen-as=./node_modules/.bin/as-proto-gen",
+        `--plugin=protoc-gen-as=${protocFile}`,
         `--proto_path=${protoDir}`,
         `--as_out=${protoDir}`,
         protoFile,
