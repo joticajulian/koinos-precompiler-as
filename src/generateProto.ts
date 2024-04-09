5a97ba2f18006ad4a3eecb2d06f0065be48b95d9
Julian Gonzalez <joticajulian@gmail.com>
2024-04-09 07:45:58 +0200

    fix: fix generateProto when using workspaces

--- src/generateProto.ts
+++ src/generateProto.ts
@@ -1,11 +1,11 @@
 import { execSync } from "child_process";
+import path from "path";
 import { getFiles } from "./utils";
 
 export function generateProto(protoDir: string): void {
-  let protocFile = "./node_modules/.bin/as-proto-gen";
-
+  let protocFile = path.join(__dirname, "../../.bin/as-proto-gen");
   if (process.platform === "win32") {
-    protocFile = ".\\node_modules\\.bin\\as-proto-gen.cmd";
+    protocFile = `${protocFile.replace(/\//g, "\\")}.cmd`;
   }
 
   const protoFiles = getFiles(protoDir, ".proto");
